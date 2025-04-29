8ff8bc523656b061d98abb731201c61366cbfe77
Shaini2003 <shinitharushika2020@gmail.com>
2025-04-29 23:09:13 +0530

    2nd commit

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function MainPage() {
   return (
-    <div>MainPage</div>
+    <div>
+        <h1>Convert your Currencies Today</h1>
+        <p>
+            Today"This application allows you to easily convert currencies based on the latest exchange rates.Whether you're planning  a trip ,managing your finances,or simply curious about the value of your money in different currencies,this tool is here to help.</p>
+    </div>
   )
 }
